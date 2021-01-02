c0eed1add6442f9654d8845977fa435902a8bcc4
guhan <guhanpal1993@gmail.com>
2021-01-02 05:11:25 +0000

    Initial Commit

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,6 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace DatingApp.API.Controllers
 {
@@ -15,12 +22,23 @@ namespace DatingApp.API.Controllers
 
         }
 
-            [HttpPost]
-        public async Task<IActionResult> Register(string username,string password){
+        [HttpPost]
+        public async Task<IActionResult> Post(UserForRegisterDto userForRegisterDto)
+        {
             //validate Request
-            username = username.ToLower();
-            if(await _repo.UserExists())
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if(await _repo.UserExists(userForRegisterDto.Username))
+            return BadRequest("Username already exists");
             
+            var userToCreate = new User{
+                Username=userForRegisterDto.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate,userForRegisterDto.Password);
+
+            return StatusCode(201);
+
         }
     }
 }
